e73a3b7c9e6e165d3012a1dd719b15cd0ba986c8
root <jesus4388>
2022-06-11 20:53:33 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,25 +2,26 @@
 /**
  * selection_sort - selection sort
  * @size: size
+ * @array: array
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t j = 0, k = 0;
 	int tmp = 0, flag = 0, pos = 0;
 
-	for(j = 0; j < size; j++)
+	for (j = 0; j < size; j++)
 	{
 		tmp = array[j];
-		for(j = j, k = j + 1; k < size; k++)
-		{	
-			if(array[k] < tmp)
+		for (j = j, k = j + 1; k < size; k++)
+		{
+			if (array[k] < tmp)
 			{
 				tmp = array[k];
 				pos = k;
 				flag = 1;
 			}
 		}
-		if(flag == 1)
+		if (flag == 1)
 		{
 			array[pos] = array[j];
 			array[j] = tmp;
