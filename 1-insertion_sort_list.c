b27764f46f255c7db4b537cd726c7ebc091e226e
root <jesus4388>
2022-06-12 09:53:42 -0700

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,35 +7,34 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux = *list;
 	listint_t *aux2 = *list;
-
-	if (list != NULL)
+	
+	if (*list == NULL)
+		return;
+	while (aux !=  NULL)
 	{
-		while (aux !=  NULL)
+		while ((aux->prev) && (aux->n < aux->prev->n) && (aux != NULL))
 		{
-			while ((aux->prev) && (aux->n < aux->prev->n) && (aux != NULL))
+			aux2 = aux->prev;
+			if (aux2->prev)
 			{
-				aux2 = aux->prev;
-				if (aux2->prev)
-				{
-					aux2->prev->next = aux;
-					aux->prev = aux2->prev;
-				}
-				else
-					aux->prev = NULL;
-				if (aux->next)
-				{
-					aux2->next = aux->next;
-					aux->next->prev = aux2;
-				}
-				else
-					aux2->next = NULL;
-				aux2->prev = aux;
-				aux->next = aux2;
-				if (aux->prev == NULL)
-					*list = aux;
-				print_list(*list);
+				aux2->prev->next = aux;
+				aux->prev = aux2->prev;
 			}
-			aux = aux->next;
+			else
+				aux->prev = NULL;
+			if (aux->next)
+			{
+				aux2->next = aux->next;
+				aux->next->prev = aux2;
+			}
+			else
+				aux2->next = NULL;
+			aux2->prev = aux;
+			aux->next = aux2;
+			if (aux->prev == NULL)
+				*list = aux;
+			print_list(*list);
 		}
+		aux = aux->next;
 	}
 }
